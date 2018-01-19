bcc6e074bd06054ca93a5d4c1bb195f64a9f5d7a
SamSmithz <teamworkzaa0129@hotmail.com>
2018-01-19 15:06:19 +0700

    BasicExample

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import BasicExample from './BasicExample';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       Hello World 
-      </div>
+      <BasicExample/>
     );
   }
 }
