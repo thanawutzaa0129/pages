347ff4bc1dda4c7c52d5cda2197496c2d945514d
SamSmithz <teamworkzaa0129@hotmail.com>
2018-01-19 16:03:07 +0700

    Add Students

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import BasicExample from './BasicExample';
+import _ from 'lodash';
+
+const students = [
+  {id : 12, name: 'Sam', score: 100},
+  {id : 18, name: 'Rose', score: 10},
+  {id : 20, name: 'Violet', score: 50}
+]
+
+const StudentLine = (props) => (
+  <div>{props.id} {props.name} = {props.score}</div>
+)
 
 class App extends Component {
   render() {
     return (
-      <BasicExample/>
+      _.map(students,s => (<StudentLine {...s} key={s.id}/>))
     );
   }
 }
